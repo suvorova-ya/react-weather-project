e19a84ee549ebb1886b6c87841a460d199614905
Yana Suvorova <suvorova_ya@outlook.com>
2023-02-24 15:01:03 +0300

    added project

---
+++ react-weather-project/src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+    method: "GET",
+    headers: {
+      'X-RapidAPI-Key': '7ddb3d1bacmsh0264b5aa8d9394fp198be5jsn6e94354c7712',
+      'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    }
+  };
+  export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+  
+  export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+  export const WEATHER_API_KEY = "3f6edc546d7ebcc59e513063e4c39217"; 
+  
